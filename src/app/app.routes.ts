ad38f2d5d5efc31c9f446d7df01628f3d7e1f208
chithra2024 <chithrasaravanan2024@gmail.com>
2025-04-17 19:12:12 +0530

    soln

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
-
-export const routes: Routes = [
-  { path: '', component: RegistrationFormComponent },
-  { path: '**', redirectTo: '' }
-];
--- src/app/models/registration-form.ts
+++
@@ -1,9 +0,0 @@
-export interface RegistrationForm {
-  name: string;
-  email: string;
-  phone: string;
-  preferredDate: Date;
-  numberOfGuests: number;
-  vehicleInterest: string;
-  specialRequirements?: string;
-}
